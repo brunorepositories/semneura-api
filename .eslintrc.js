5cdcac9186f3cc46cfaa09ba2e258d73b42b7134
brunorepositories <hs.bruno@outlook.com>
2020-09-02 01:06:21 -0300

    ajustando es-lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,8 @@ module.exports = {
         "node": true
     },
     "extends": [
-        "standard"
+        "standard",
+        "plugin:@typescript-eslint/recommended"
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
